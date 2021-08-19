722e04a658a696724952552f5dd26195ebbaf544
Aigerim Ingco <aikish20@yahoo.com>
2021-08-18 18:36:29 -0600

    task practice

--- src/test/java/com/cybertek/step_definitions/Hooks.java
+++ src/test/java/com/cybertek/step_definitions/Hooks.java
@@ -1,9 +1,9 @@
 package com.cybertek.step_definitions;
 
-import io.cucumber.java.After;
-import io.cucumber.java.AfterStep;
-import io.cucumber.java.Before;
-import io.cucumber.java.BeforeStep;
+import com.cybertek.utilities.Driver;
+import io.cucumber.java.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
    //IMPORT FROM IO.CUCUMBER.JAVA
@@ -11,10 +11,22 @@ public class Hooks {
     public void setupScenario(){
         System.out.println("Setting up browser with further details...");
     }
+
     @After
-    public void tearDownScenario(){
-        System.out.println("--Teardown steps are being applied...");
-       // Driver.closeDriver();
+    public void tearDownScenario(Scenario scenario){
+        //IF MY SCENARIO FAILS
+        //TAKE A SCREENSHOT
+
+        //scenario.isFailed();
+        if(scenario.isFailed()){
+            byte [] screenshot = ((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png",scenario.getName());
+        }
+
+
+
+        System.out.println("AFTER--Teardown steps are being applied...");
+        // Driver.closeDriver();
     }
     @BeforeStep
     public void setupStep(){
