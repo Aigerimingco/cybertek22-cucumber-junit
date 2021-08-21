be4a13a5f66f2a49193f68accaf647d503d46f8d
Aigerim Ingco <aikish20@yahoo.com>
2021-08-21 10:07:14 -0600

    browser utils

--- src/test/java/com/cybertek/utilities/BrowserUtils.java
+++ src/test/java/com/cybertek/utilities/BrowserUtils.java
@@ -1,6 +1,38 @@
 package com.cybertek.utilities;
 
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BrowserUtils {
+/**
+ * This methods accepts String expected title
+ * @param expectedTitle
+ *
+ */
+    public static void assertTitle(String expectedTitle){
+        String actualTitle= Driver.getDriver().getTitle();
+        Assert.assertEquals(expectedTitle,actualTitle);
+    }
+
+
+
+    /**
+    This methods accepts a List<WebElement> and returns lIST<String>
+     @param webElementList
+     */
+    public static List<String> getElementsText(List<WebElement> webElementList){
+        //create placeholder List<String>
+        List<String> actualAsString= new ArrayList<>();
+
+        for (WebElement each : webElementList) {
+            actualAsString.add(each.getText());
+
+        }
+        return actualAsString;
+    }
 
     public static void sleep(int second)  {
         second*=1000;
